1cba6a1e7616cb2d5b94d03d302d524aa8a267b2
Erhan YILDIRIM <erhan-yildirim@outlook.com>
2023-12-17 23:22:01 +0300

    Manav kasa programi

---
+++ ManavKasaProgrami/src/JavaOdev/Odev_Class.java
@@ -0,0 +1,12 @@
+package JavaOdev;
+
+public class Odev_Class {
+    /**
+     * @author Erhan YILDIRIM
+     */
+
+    public static void main(String[] args) {
+        String kelimeler = "Merhaba Patika, ben Erhan.";
+        System.out.print(kelimeler);
+    }
+}
